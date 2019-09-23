2fa0e47be775db74c95de86ccf3bd54462f6e2d2
Jannis Gebauer <ja.geb@me.com>
2019-09-23 10:06:45 +0200

    update app

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/apps.py
@@ -5,9 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class AppConfig(_AppConfig):
     name = "{{ cookiecutter.project_slug }}.app"
     verbose_name = _("App")
-
-    def ready(self):
-        try:
-            import {{ cookiecutter.project_slug }}.users.signals  # noqa F401
-        except ImportError:
-            pass
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/urls.py
@@ -4,7 +4,7 @@ from {{ cookiecutter.project_slug }}.app.views import (
     AppHomeView
 )
 
-app_name = "users"
+app_name = "app"
 urlpatterns = [
     path("", view=AppHomeView.as_view(), name="home"),
     path("users/", include("{{ cookiecutter.project_slug }}.users.urls", namespace="users")),
