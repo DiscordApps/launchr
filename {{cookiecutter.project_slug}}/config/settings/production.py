b5fa75315e5c9d06410fddc7a6e76c1af85ee265
Jannis Gebauer <ja.geb@me.com>
2019-10-08 15:32:41 +0200

    added django and django_failover to allowed hosts

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -17,6 +17,8 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["{{ cookiecutter.domain_name }}"])
+# for caddy internal health checks
+ALLOWED_HOSTS.extend(["django", "django_failover"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
